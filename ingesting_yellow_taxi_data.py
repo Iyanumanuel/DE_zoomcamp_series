141c5623ba6c4dc2f72d09fea3b62ac0bcb5f219
iyanumanuel <e.iyanuloluwa@gmail.com>
2022-02-28 23:48:42 +0100

    updated ingestion script

--- ingesting_yellow_taxi_data.py
+++ ingesting_yellow_taxi_data.py
@@ -35,28 +35,3 @@ while True:
   t_end = time()
 
 print('inserted another chunk..., took %.3f second'%(t_end - t_start))
-
-
-
-
-# df = pd.read_csv('https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-01.csv', nrows=100)
-
-# print(pd.io.sql.get_schema(df, name='yellow_taxi_data',  con=engine))
-
-# df_iter = pd.read_csv('https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-01.csv', iterator=True, chunksize= 100000)
-
-# df = next(df_iter)
-
-# len(df)
-
-# df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
-
-# while True:
-#   t_start = time()
-#   df = next(df_iter)
-#   df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-#   df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-#   df.to_sql(con=engine, name='yellow_taxi_data', if_exists='append')
-#   t_end = time()
-
-#   print('inserted another chunk..., took %.3f second'%(t_end -t_start))
